90fcffca0dd9858109d4ed63d9f557120822fd94
Dzung Nguyen <dzung_min@yahoo.com>
2020-06-27 04:50:36 +0700

    Add simple drawing function

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ for (let i = 0; i < 16; i++) {
     table.appendChild(cell);
   }
 }
+
+const cells = document.querySelectorAll('.cell');
+let counter = 0;
+table.addEventListener('click', () => {
+  counter++;
+  cells.forEach(cell => {
+    if (counter % 2 === 1) {
+      cell.addEventListener('mouseover', (e) => {
+        e.target.classList.add('painted');
+      })
+    } else if (cell.className.indexOf('painted') === -1) {
+      cell.addEventListener('mouseover', (e) => {
+        e.target.className = 'cell';
+      })
+    }
+  })
+})

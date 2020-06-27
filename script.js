df5a4ef1d0e040bec7fa9ee2668713a78c967c97
Dzung Nguyen <dzung_min@yahoo.com>
2020-06-27 21:51:47 +0700

    working version with basic style

--- script.js
+++ script.js
@@ -1,28 +1,81 @@
-const table = document.querySelector('#container');
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    table.appendChild(cell);
+/************************************************
+ * MAIN PROGRAM
+ ************************************************/
+
+let defaultSize = 16;
+let drawingStage = false;
+
+const newBtn = document.querySelector('#new-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const board = document.querySelector('#container');
+
+board.addEventListener('dragstart', (e) => { e.preventDefault(); })
+board.addEventListener('mouseleave', stopDrawing)
+
+document.addEventListener('DOMContentLoaded', () => {
+  draw(createBoard(defaultSize));
+});
+
+newBtn.addEventListener('click', () => {
+  let newSize = null;
+  do {
+    newSize = +prompt('Enter board size');
+  } while (isNaN(newSize));
+  if (!newSize) {
+    newSize = defaultSize;
+  }
+  draw(createBoard(newSize));
+})
+
+resetBtn.addEventListener('click', resetBoard);
+
+
+/**********************************************
+ * HELPER FUNCTIONS
+ **********************************************/
+
+function createBoard(size) {
+  // Create a new board and return a nodeList of cells in that board
+  board.innerHTML = ''; // Clear old board
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell')
+      board.appendChild(cell);
+    }
+  }
+  board.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
+  return document.querySelectorAll('.cell');
+}
+
+function draw(cellList) {
+  cellList.forEach(cell => {
+    cell.addEventListener('mousedown', startDrawing);
+    cell.addEventListener('mousemove', drawing);
+    cell.addEventListener('mouseup', stopDrawing);
+  });
+}
+
+function startDrawing(e) {
+  drawingStage = true;
+  e.target.classList.add('drawed');
+
+}
+
+function drawing(e) {
+  let currentClass = Array.from(e.target.classList);
+  if (currentClass.indexOf('drawed') === -1 && drawingStage == true) {
+    e.target.classList.add('drawed');
   }
 }
 
-// Can't find out how to click and drag the mouse to draw yet
-// Temporary solution: When the user click -> start drawing, then
-// stop when the user click again.
-const cells = document.querySelectorAll('.cell');
-let counter = 0;
-table.addEventListener('click', () => {
-  counter++;
+function stopDrawing(e) {
+  drawingStage = false;
+}
+
+function resetBoard() {
+  const cells = document.querySelectorAll('.drawed');
   cells.forEach(cell => {
-    if (counter % 2 === 1) {
-      cell.addEventListener('mouseover', (e) => {
-        e.target.classList.add('painted');
-      })
-    } else if (cell.className.indexOf('painted') === -1) {
-      cell.addEventListener('mouseover', (e) => {
-        e.target.className = 'cell';
-      })
-    }
+    cell.className = 'cell';
   })
-})
+}
